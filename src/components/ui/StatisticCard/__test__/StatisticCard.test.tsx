d2ab3fb4ffb5fb2be8b46fb71ffb116ac3dc1b0b
Thao Ha <thao.ha@asnet.com.vn>
2024-12-04 17:58:41 +0700

    feat: implement UI for wallet page

--- src/components/ui/StatisticCard/__test__/StatisticCard.test.tsx
+++ src/components/ui/StatisticCard/__test__/StatisticCard.test.tsx
@@ -3,14 +3,14 @@ import { StatisticCard } from '..';
 
 describe('StatisticCard Component', () => {
   it('renders the title and value correctly', () => {
-    render(<StatisticCard title="VHA Token Balance" value="123" />);
+    render(<StatisticCard title="VHA Token Balance" value={123} />);
 
     expect(screen.getByText('VHA Token Balance')).toBeInTheDocument();
-    expect(screen.getByText('123')).toBeInTheDocument();
+    expect(screen.getByText('$123.00')).toBeInTheDocument();
   });
 
   it('applies the default custom color when no customColor is provided', () => {
-    render(<StatisticCard title="Default Color Test" value="456" />);
+    render(<StatisticCard title="Default Color Test" value={456} />);
 
     const spanElement = screen.getByTestId('statistic-card');
     expect(spanElement).toHaveClass('bg-secondary-100');
@@ -21,7 +21,7 @@ describe('StatisticCard Component', () => {
     render(
       <StatisticCard
         title="Custom Color Test"
-        value="789"
+        value={789}
         customColor={customColor}
       />,
     );
--- src/components/ui/StatisticCard/index.tsx
+++ src/components/ui/StatisticCard/index.tsx
@@ -1,8 +1,11 @@
 import { Text } from '@/components/ui';
 
+// Utils
+import { formatNumber } from '@/utils';
+
 interface StatisticCardProps {
   title: string;
-  value: string;
+  value: number;
   customColor?: string;
 }
 
@@ -11,14 +14,14 @@ export const StatisticCard = ({
   value,
   customColor = 'bg-secondary-100',
 }: StatisticCardProps) => (
-  <section className="relative bg-background-200 py-[14px] px-5 rounded-xl w-[236px] h-[110px]">
+  <section className="relative bg-background-200 py-[14px] px-5 rounded-xl w-[336px] h-[130px]">
     <span
       data-testid="statistic-card"
       className={`absolute top-4 left-0 w-[5px] h-[53px] rounded-r-lg ${customColor}`}
     />
     <Text customClass="text-primary-300 text-sm font-semibold">{title}</Text>
     <Text variant="title" customClass="mt-[10px] text-2xl font-semibold">
-      {value}
+      ${formatNumber(value)}
     </Text>
   </section>
 );
